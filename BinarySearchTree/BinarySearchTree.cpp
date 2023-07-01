95c61be8f44e42a7c10e8109f8ce8b08ab31615e
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2023-07-01 12:24:27 +0700

    member postorder

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -101,4 +101,18 @@ public:
 		}
 	}
 
+	void postorder(node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << " ";
+		}
+	}
 };
